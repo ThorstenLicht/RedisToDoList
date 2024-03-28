02a7a99612960028753239f5166e5901f9dbccb9
ThorstenLicht <114730064+ThorstenLicht@users.noreply.github.com>
2024-03-28 14:02:07 +0100

    EntiresAfterLogIn commented back

--- backend/src/wsFunctions/EntriesAfterLogIn.ts
+++ backend/src/wsFunctions/EntriesAfterLogIn.ts
@@ -1,17 +1,17 @@
-// import ws from "ws";
-// import getEntries from "../RedisFunctions/GetEntries";
-// import { addError } from "../AddMessagetypToString";
+import ws from "ws";
+import getEntries from "../RedisFunctions/GetEntries";
+import { addError } from "../AddMessagetypToString";
 
-// async function entriesAfterLogIn(connection: ws) {
-//   try {
-//     const allEvents = await getEntries();
-//     const send = JSON.stringify(allEvents);
-//     connection.send(send);
-//   } catch (error) {
-//     console.error("Error:", error);
-//     const send = JSON.stringify(addError("Es ist ein Fehler aufgetreten"));
-//     connection.send(send);
-//   }
-// }
+async function entriesAfterLogIn(connection: ws) {
+  try {
+    const allEvents = await getEntries();
+    const send = JSON.stringify(allEvents);
+    connection.send(send);
+  } catch (error) {
+    console.error("Error:", error);
+    const send = JSON.stringify(addError("Es ist ein Fehler aufgetreten"));
+    connection.send(send);
+  }
+}
 
-// export default entriesAfterLogIn;
+export default entriesAfterLogIn;
